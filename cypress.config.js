eb04d4ae035da274fd53390dd494173d1ffdede2
Saim428 <saim.jahangir@arbisoft.com>
2023-04-27 12:37:11 +0500

    Changes related to running all tests instead on only 1

--- cypress.config.js
+++ cypress.config.js
@@ -11,7 +11,7 @@ module.exports = defineConfig({
           chromeWebSecurity: false
         }
       }
-      if (config.env.ENVIRONMENT == "ExamAPPTests"){
+      else if (config.env.ENVIRONMENT == "ExamAPPTests"){
         return{
           baseUrl:"https://exam-template-refactoring.herokuapp.com?examId=6290b696cb386f0004cbef07&token=Bearer%20eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzaGVoc2F3YXIuYWxpKzQ2QGFyYmlzb2Z0LmNvbSIsInNjb3BlcyI6WyJWRVJJRklFRCJdfQ.i3HqDp_yy2XIUZLIJL6UbplGF81_FoO9u_XC6fCdBUZIuRonI_JrafhfN1sBVFJziCaYzuk1g5tRgoh_EYez4w",
           specPattern: "cypress/e2e/ExamAPPTests/**/*.cy.js",
